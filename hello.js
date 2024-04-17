74adc0baeaa7d99b700867bbd2572db0979c53e6
Jason Newsom <jnewsom405@gmail.com>
2024-04-17 18:02:45 -0500

    3rd commit

--- hello.js
+++ hello.js
@@ -1,1 +1,4 @@
-// this is a test file
+import { color, log, red, green } from 'console-log-colors';
+
+console.log(color.red("why am I here?! because you deserve to be!"));
+console.log(color.green("you WILL succeed at this and find a new job!"))
---
+++ index.js
@@ -0,0 +1,3 @@
+import { color, log, red, green } from 'console-log-colors';
+
+console.log("hello you!")
