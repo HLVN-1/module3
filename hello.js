328637134c4f39d343eb6679b68200d97c3db8ea
Jason Newsom <jnewsom405@gmail.com>
2024-04-17 18:27:22 -0500

    Going through holy trinity higher order functions

--- hello.js
+++ hello.js
@@ -2,3 +2,41 @@ import { color, log, red, green } from 'console-log-colors';
 
 console.log(color.red("why am I here?! because you deserve to be!"));
 console.log(color.green("you WILL succeed at this and find a new job!"))
+
+const numbers = [1, 2, 3, 4, 5];
+
+// function addOne(array){
+//     for (let i = 0; i < array.length; i++){
+//         console.log(color.red(array[i] + 1));
+//     }
+// }
+
+// addOne(numbers);
+
+// these two are equal
+
+numbers.forEach(function(num){
+    console.log(color.green(num + 1));
+});
+
+numbers.forEach((num) => {console.log(color.red(num + 1))});
+
+
+// Map Reduce Filter = the holy trinity of Higher Order Functions
+
+// Filter
+
+// function isOdd(array, oddArr = []) {
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] % 2 !== 0) {
+//             oddArr.push(array[i]);
+//         }
+//     }
+//     return oddArr;
+// }
+
+// const oddArray = isOdd(numbers);
+// console.log(oddArray);
+
+const easyOddArray = numbers.filter((num) => num % 2 !== 0 );
+console.log(easyOddArray);
